mjsmar/qemu-dov-kernel-hashes-v1

spapr: add helper to retrieve a PHB/device DrcEntry
Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

hw/ppc/spapr.c
@@ -239,6 +239,42 @@ static size_t create_page_sizes_prop(CPUPPCState *env, uint32_t *prop,
     return (p - prop) * sizeof(uint32_t);
 }
 
+DrcEntry *spapr_phb_to_drc_entry(uint64_t buid)
+{
+    int i;
+
+    for (i = 0; i < SPAPR_DRC_TABLE_SIZE; i++) {
+        if (drc_table[i].phb_buid == buid) {
+            return &drc_table[i];
+        }
+     }
+
+     return NULL;
+}
+
+DrcEntry *spapr_find_drc_entry(int drc_index)
+{
+    int i, j;
+
+    for (i = 0; i < SPAPR_DRC_TABLE_SIZE; i++) {
+        DrcEntry *phb_entry = &drc_table[i];
+        if (phb_entry->drc_index == drc_index) {
+            return phb_entry;
+        }
+        if (phb_entry->child_entries == NULL) {
+            continue;
+        }
+        for (j = 0; j < SPAPR_DRC_PHB_SLOT_MAX; j++) {
+            DrcEntry *entry = &phb_entry->child_entries[j];
+            if (entry->drc_index == drc_index) {
+                return entry;
+            }
+        }
+     }
+
+     return NULL;
+}
+
 static void spapr_init_drc_table(void)
 {
     int i;

include/hw/ppc/spapr.h
@@ -346,6 +346,8 @@ struct DrcEntry {
 
 extern DrcEntry drc_table[SPAPR_DRC_TABLE_SIZE];
 DrcEntry *spapr_add_phb_to_drc_table(uint64_t buid, uint32_t state);
+DrcEntry *spapr_phb_to_drc_entry(uint64_t buid);
+DrcEntry *spapr_find_drc_entry(int drc_index);
 
 
 extern sPAPREnvironment *spapr;

schabrolles/qemu

hw/ppc/spapr.c
@@ -282,6 +282,29 @@ sPAPRDrcEntry *spapr_phb_to_drc_entry(uint64_t buid)
      return NULL;
 }
 
+sPAPRDrcEntry *spapr_find_drc_entry(int drc_index)
+{
+    int i, j;
+
+    for (i = 0; i < SPAPR_DRC_TABLE_SIZE; i++) {
+        sPAPRDrcEntry *phb_entry = &spapr->drc_table[i];
+        if (phb_entry->drc_index == drc_index) {
+            return phb_entry;
+        }
+        if (phb_entry->child_entries == NULL) {
+            continue;
+        }
+        for (j = 0; j < SPAPR_DRC_PHB_SLOT_MAX; j++) {
+            sPAPRDrcEntry *entry = &phb_entry->child_entries[j];
+            if (entry->drc_index == drc_index) {
+                return entry;
+            }
+        }
+     }
+
+     return NULL;
+}
+
 static void spapr_init_drc_table(void)
 {
     int i;

include/hw/ppc/spapr.h
@@ -452,5 +452,6 @@ int spapr_tcet_dma_dt(void *fdt, int node_off, const char *propname,
                       sPAPRTCETable *tcet);
 sPAPRDrcEntry *spapr_add_phb_to_drc_table(uint64_t buid, uint32_t state);
 sPAPRDrcEntry *spapr_phb_to_drc_entry(uint64_t buid);
+sPAPRDrcEntry *spapr_find_drc_entry(int drc_index);
 
 #endif /* !defined (__HW_SPAPR_H__) */