70f62371d8717192904ec90442b05d569bedff9f
Peter Crosthwaite <crosthwaitepeter@gmail.com>
2015-07-06 12:15:12 -0600

    cpu-all: complete "real" host page size API
    Currently the "host" page size alignment API is really aligning to both
    host and target page sizes. There is the qemu_real_page_size which can
    be used for the actual host page size but it's missing a mask and ALIGN
    macro as provided for qemu_page_size. Complete the API. This allows
    system level code that cares about the host page size to use a
    consistent alignment interface without having to un-needingly align to
    the target page size. This also reduces system level code dependency
    on the cpu specific TARGET_PAGE_SIZE.
    
    Signed-off-by: Peter Crosthwaite <crosthwaite.peter@gmail.com>
    Tested-by: Alexey Kardashevskiy <aik@ozlabs.ru>
    Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>
    (cherry picked from commit 4e51361d79289aee2985dfed472f8d87bd53a8df)
    Signed-off-by: Alexey Kardashevskiy <aik@ozlabs.ru>

--- translate-all.c
+++ translate-all.c
@@ -116,6 +116,7 @@ typedef struct PageDesc {
 #define V_L1_SHIFT (L1_MAP_ADDR_SPACE_BITS - TARGET_PAGE_BITS - V_L1_BITS)
 
 uintptr_t qemu_real_host_page_size;
+uintptr_t qemu_real_host_page_mask;
 uintptr_t qemu_host_page_size;
 uintptr_t qemu_host_page_mask;
 
@@ -305,6 +306,7 @@ void page_size_init(void)
     /* NOTE: we can always suppose that qemu_host_page_size >=
        TARGET_PAGE_SIZE */
     qemu_real_host_page_size = getpagesize();
+    qemu_real_host_page_mask = ~(qemu_real_host_page_size - 1);
     if (qemu_host_page_size == 0) {
         qemu_host_page_size = qemu_real_host_page_size;
     }
