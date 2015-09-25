f07cb7d8bb3e87c2ef25231c27d2bf8095dbb3a5
Thomas Huth <thuth@redhat.com>
2015-09-25 15:53:14 +0200

    ppc/spapr: Implement H_RANDOM hypercall in QEMU
    The PAPR interface defines a hypercall to pass high-quality
    hardware generated random numbers to guests. Recent kernels can
    already provide this hypercall to the guest if the right hardware
    random number generator is available. But in case the user wants
    to use another source like EGD, or QEMU is running with an older
    kernel, we should also have this call in QEMU, so that guests that
    do not support virtio-rng yet can get good random numbers, too.
    
    This patch now adds a new pseudo-device to QEMU that either
    directly provides this hypercall to the guest or is able to
    enable the in-kernel hypercall if available. The in-kernel
    hypercall can be enabled with the use-kvm property, e.g.:
    
     qemu-system-ppc64 -device spapr-rng,use-kvm=true
    
    For handling the hypercall in QEMU instead, a "RngBackend" is
    required since the hypercall should provide "good" random data
    instead of pseudo-random (like from a "simple" library function
    like rand() or g_random_int()). Since there are multiple RngBackends
    available, the user must select an appropriate back-end via the
    "rng" property of the device, e.g.:
    
     qemu-system-ppc64 -object rng-random,filename=/dev/hwrng,id=gid0 \
                       -device spapr-rng,rng=gid0 ...
    
    See http://wiki.qemu-project.org/Features-Done/VirtIORNG for
    other example of specifying RngBackends.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    (cherry picked from commit 4d9392be6c1aada69ce86c0f6584128976985394 in
     David Gibson's tree at github, fixed a few conflicts)
    
    *Fixes BZ 130265
    
    Signed-off-by: Greg Kurz <gkurz@linux.vnet.ibm.com>
    Signed-off-by: Alexey Kardashevskiy <aik@ozlabs.ru>

--- target-ppc/kvm.c
+++ target-ppc/kvm.c
@@ -2510,3 +2510,12 @@ int kvm_arch_fixup_msi_route(struct kvm_irq_routing_entry *route,
 {
     return 0;
 }
+
+int kvmppc_enable_hwrng(void)
+{
+    if (!kvm_enabled() || !kvm_check_extension(kvm_state, KVM_CAP_PPC_HWRNG)) {
+        return -1;
+    }
+
+    return kvmppc_enable_hcall(kvm_state, H_RANDOM);
+}
