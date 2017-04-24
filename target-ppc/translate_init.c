820a73e288d7d856767f840432885b96539a40fd
Alexey Kardashevskiy <aik@ozlabs.ru>
2017-04-24 14:49:48 +1000

    Revert "target/ppc/POWER9: Add ISAv3.00 MMU definition"
    This reverts commit 9a5fb018323142dfad241f5237bc62c8eb5c1d1f.

--- target-ppc/translate_init.c
+++ target-ppc/translate_init.c
@@ -8836,7 +8836,8 @@ POWERPC_FAMILY(POWER9)(ObjectClass *oc, void *data)
                     (1ull << MSR_PMM) |
                     (1ull << MSR_RI) |
                     (1ull << MSR_LE);
-    pcc->mmu_model = POWERPC_MMU_3_00;
+    /* Using 2.07 defines until new radix model is added. */
+    pcc->mmu_model = POWERPC_MMU_2_07;
 #if defined(CONFIG_SOFTMMU)
     pcc->handle_mmu_fault = ppc_hash64_handle_mmu_fault;
     /* segment page size remain the same */
