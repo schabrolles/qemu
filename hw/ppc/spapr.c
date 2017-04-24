f2e5c176a5168e9fe4cc2011362c14e28bed17f2
Alexey Kardashevskiy <aik@ozlabs.ru>
2017-04-24 14:49:47 +1000

    Revert "spapr: Small cleanup of PPC MMU enums (partial)"
    This reverts commit e187d3601d35ef9c2acea9eee2f8cf8050094453.

--- hw/ppc/spapr.c
+++ hw/ppc/spapr.c
@@ -373,16 +373,18 @@ static void spapr_populate_pa_features(CPUPPCState *env, void *fdt, int offset)
     uint8_t *pa_features;
     size_t pa_size;
 
-    switch (POWERPC_MMU_VER(env->mmu_model)) {
-    case POWERPC_MMU_VER_2_06:
+    switch (env->mmu_model) {
+    case POWERPC_MMU_2_06:
+    case POWERPC_MMU_2_06a:
         pa_features = pa_features_206;
         pa_size = sizeof(pa_features_206);
         break;
-    case POWERPC_MMU_VER_2_07:
+    case POWERPC_MMU_2_07:
+    case POWERPC_MMU_2_07a:
         pa_features = pa_features_207;
         pa_size = sizeof(pa_features_207);
         break;
-    case POWERPC_MMU_VER_3_00:
+    case POWERPC_MMU_3_00:
         pa_features = pa_features_300;
         pa_size = sizeof(pa_features_300);
         break;
