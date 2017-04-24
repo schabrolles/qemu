060ad6cce32f1c343681962042213560f92d44e7
Alexey Kardashevskiy <aik@ozlabs.ru>
2017-04-24 14:49:51 +1000

    Revert "9pfs: local: mkdir: don't follow symlinks"
    This reverts commit 2fb01dc6a97575cdfc4b29d0d707454fec35ecae.

--- hw/9pfs/9p-local.c
+++ hw/9pfs/9p-local.c
@@ -800,47 +800,62 @@ out:
 static int local_mkdir(FsContext *fs_ctx, V9fsPath *dir_path,
                        const char *name, FsCred *credp)
 {
+    char *path;
     int err = -1;
-    int dirfd;
+    int serrno = 0;
+    V9fsString fullname;
+    char *buffer = NULL;
 
-    dirfd = local_opendir_nofollow(fs_ctx, dir_path->data);
-    if (dirfd == -1) {
-        return -1;
-    }
+    v9fs_string_init(&fullname);
+    v9fs_string_sprintf(&fullname, "%s/%s", dir_path->data, name);
+    path = fullname.data;
 
-    if (fs_ctx->export_flags & V9FS_SM_MAPPED ||
-        fs_ctx->export_flags & V9FS_SM_MAPPED_FILE) {
-        err = mkdirat(dirfd, name, SM_LOCAL_DIR_MODE_BITS);
+    /* Determine the security model */
+    if (fs_ctx->export_flags & V9FS_SM_MAPPED) {
+        buffer = rpath(fs_ctx, path);
+        err = mkdir(buffer, SM_LOCAL_DIR_MODE_BITS);
         if (err == -1) {
             goto out;
         }
-        credp->fc_mode = credp->fc_mode | S_IFDIR;
-
-        if (fs_ctx->export_flags & V9FS_SM_MAPPED) {
-            err = local_set_xattrat(dirfd, name, credp);
-        } else {
-            err = local_set_mapped_file_attrat(dirfd, name, credp);
-        }
+        credp->fc_mode = credp->fc_mode|S_IFDIR;
+        err = local_set_xattr(buffer, credp);
         if (err == -1) {
+            serrno = errno;
             goto err_end;
         }
-    } else if (fs_ctx->export_flags & V9FS_SM_PASSTHROUGH ||
-               fs_ctx->export_flags & V9FS_SM_NONE) {
-        err = mkdirat(dirfd, name, credp->fc_mode);
+    } else if (fs_ctx->export_flags & V9FS_SM_MAPPED_FILE) {
+        buffer = rpath(fs_ctx, path);
+        err = mkdir(buffer, SM_LOCAL_DIR_MODE_BITS);
         if (err == -1) {
             goto out;
         }
-        err = local_set_cred_passthrough(fs_ctx, dirfd, name, credp);
+        credp->fc_mode = credp->fc_mode|S_IFDIR;
+        err = local_set_mapped_file_attr(fs_ctx, path, credp);
         if (err == -1) {
+            serrno = errno;
+            goto err_end;
+        }
+    } else if ((fs_ctx->export_flags & V9FS_SM_PASSTHROUGH) ||
+               (fs_ctx->export_flags & V9FS_SM_NONE)) {
+        buffer = rpath(fs_ctx, path);
+        err = mkdir(buffer, credp->fc_mode);
+        if (err == -1) {
+            goto out;
+        }
+        err = local_post_create_passthrough(fs_ctx, path, credp);
+        if (err == -1) {
+            serrno = errno;
             goto err_end;
         }
     }
     goto out;
 
 err_end:
-    unlinkat_preserve_errno(dirfd, name, AT_REMOVEDIR);
+    remove(buffer);
+    errno = serrno;
 out:
-    close_preserve_errno(dirfd);
+    g_free(buffer);
+    v9fs_string_free(&fullname);
     return err;
 }
 
